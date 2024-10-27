9893a8c48bb18e7a99327a44318c7226dcf6b35b
hurtadx <andreshm253@gmail.com>
2024-10-27 16:15:37 -0500

    Fix Center Problems

--- js/script.js
+++ js/script.js
@@ -1,29 +1,28 @@
-// script.js actualizado
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('.btnLogin-popup');
 const iconClose = document.querySelector('.icon-close');
 
-// Muestra el registro
+
 registerLink.addEventListener('click', () => {
     wrapper.classList.add('active');
-    wrapper.classList.add('active-popup'); // Muestra centrado
+    wrapper.classList.add('active-popup'); 
 });
 
-// Muestra el inicio de sesión
+
 loginLink.addEventListener('click', () => {
     wrapper.classList.remove('active');
-    wrapper.classList.add('active-popup'); // Muestra centrado
+    wrapper.classList.add('active-popup'); 
 });
 
-// Muestra el popup al hacer clic en "Iniciar Sesión"
+
 btnPopup.addEventListener('click', () => {
     wrapper.classList.add('active-popup');
     wrapper.style.display = 'flex'; 
 });
 
-// Cierra el popup y oculta el `wrapper`
+
 iconClose.addEventListener('click', () => {
     wrapper.classList.remove('active-popup');
     setTimeout(() => {
