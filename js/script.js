b239351040d6c5a2e65930388114c23574666058
hurtadx <andreshm253@gmail.com>
2024-10-27 14:47:23 -0500

    Fix Popup Login

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// script.js actualizado
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
@@ -7,24 +8,26 @@ const iconClose = document.querySelector('.icon-close');
 // Muestra el registro
 registerLink.addEventListener('click', () => {
     wrapper.classList.add('active');
+    wrapper.classList.add('active-popup'); // Muestra centrado
 });
 
 // Muestra el inicio de sesión
 loginLink.addEventListener('click', () => {
     wrapper.classList.remove('active');
+    wrapper.classList.add('active-popup'); // Muestra centrado
 });
 
-// Muestra el popup
+// Muestra el popup al hacer clic en "Iniciar Sesión"
 btnPopup.addEventListener('click', () => {
     wrapper.classList.add('active-popup');
-    wrapper.style.display = 'flex'; // Asegúrate de que el popup sea visible
+    wrapper.style.display = 'flex'; 
 });
 
-// Cierra el popup
+// Cierra el popup y oculta el `wrapper`
 iconClose.addEventListener('click', () => {
     wrapper.classList.remove('active-popup');
     setTimeout(() => {
-        wrapper.classList.remove('active'); // También remueve la clase 'active' si es necesario
-        wrapper.style.display = 'none'; // Oculta el wrapper después de que se cierra
-    }, 50); // Asegúrate de que el tiempo coincide con la duración de la transición
+        wrapper.classList.remove('active');
+        wrapper.style.display = 'none';
+    }, 50); 
 });
