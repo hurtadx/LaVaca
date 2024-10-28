63092d91a3f491e40ff23fc37eb245b0538bb7cb
hurtadx <andreshm253@gmail.com>
2024-10-27 21:05:00 -0500

    close login fix

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const registerLink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('.btnLogin-popup');
 const iconClose = document.querySelector('.icon-close');
 
-// Inicializa el estado del wrapper basado en localStorage
+
 document.addEventListener('DOMContentLoaded', () => {
     const isWrapperOpen = localStorage.getItem('isWrapperOpen') === 'true';
     wrapper.style.display = isWrapperOpen ? 'flex' : 'none';
@@ -13,32 +13,32 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 });
 
-// Cuando se hace clic en el enlace de registro
+
 registerLink.addEventListener('click', () => {
     wrapper.classList.add('active');
     wrapper.classList.add('active-popup'); 
 });
 
-// Cuando se hace clic en el enlace de inicio de sesión
+
 loginLink.addEventListener('click', () => {
     wrapper.classList.remove('active');
     wrapper.classList.add('active-popup'); 
 });
 
-// Cuando se hace clic en el botón de inicio de sesión
+
 btnPopup.addEventListener('click', () => {
     wrapper.classList.add('active-popup');
     wrapper.style.display = 'flex';
-    localStorage.setItem('isWrapperOpen', 'true'); // Guardar estado de visibilidad
+    localStorage.setItem('isWrapperOpen', 'true'); 
 });
 
-// Cuando se hace clic en el icono de cerrar
+
 iconClose.addEventListener('click', () => {
     wrapper.classList.remove('active-popup');
     setTimeout(() => {
         wrapper.classList.remove('active');
         wrapper.style.display = 'none';
-        localStorage.setItem('isWrapperOpen', 'false'); // Guardar estado de cerrado
+        localStorage.setItem('isWrapperOpen', 'false'); 
     }, 50); 
 });
 
